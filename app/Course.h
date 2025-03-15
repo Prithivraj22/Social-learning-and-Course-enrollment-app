52f0575291c1bafa9f362990799858ccc4124155
Prithivraj22 <135855853+Prithivraj22@users.noreply.github.com>
2025-03-15 21:37:16 +0530

    Create Course.h

---
+++ app/Course.h
@@ -0,0 +1,17 @@
+#ifndef COURSE_H
+#define COURSE_H
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Course {
+public:
+    int id;
+    string title, category, tutor, syllabus;
+    int difficulty;
+    double price;
+    float rating;
+    vector<string> reviews;
+    
+    Course(int id, string title, string category, string tutor, string syllabus, int difficulty, double price, float rating);
+};
